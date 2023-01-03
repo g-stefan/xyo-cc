92a5f5604aa6f20f2727b86e5ed5c2a9baad2794
Grigore Stefan <g_stefan@yahoo.com>
2023-01-03 21:13:40 +0200

    Update

--- fabricare/test.js
+++ fabricare/test.js
@@ -3,10 +3,6 @@
 // SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
 
-if(Project.name!="xyo-cc"){
-	return;
-};
-
 messageAction("test");
 
 Shell.mkdirRecursivelyIfNotExists("output/test");
